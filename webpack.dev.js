88f145f6d10fa3499726c8b8d2d33e89e590eff0
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-04-15 20:32:20 -0700

    fix: add hot reload to the webpack template

--- webpack.dev.js
+++ webpack.dev.js
@@ -23,6 +23,16 @@ module.exports = {
   ],
 
   devServer: {
+    mode: "development",
+
+    devServer: {
+      static: "./dist", // folder to serve
+      open: true, // auto opens browser
+      hot: true, // enables hot reload
+      liveReload: true, // fallback reload
+      port: 3000, // optional
+    },
+
     client: {
       overlay: false,
     },
